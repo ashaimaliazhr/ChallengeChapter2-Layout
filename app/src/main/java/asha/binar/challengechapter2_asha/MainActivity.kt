d2f39236b9cfe8c2ccfe30f38c042709fba6ced3
ashaimaliazhr <asha.imaliazahra2006@students.unila.ac.id>
2022-08-27 08:44:41 +0700

    Initial commit

---
+++ app/src/main/java/asha/binar/challengechapter2_asha/MainActivity.kt
@@ -0,0 +1,11 @@
+package asha.binar.challengechapter2_asha
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.challenge_chap2)
+    }
+}
